6b8902b6bb26bb659f17fd5f4b6ac0c5dcfa6b1a
jaehoon-Han <eroel1060@daum.net>
2022-05-24 16:27:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-
+df1 = pd.read_csv('data/NL_bo_sense_2019.csv')
+df2 = pd.read_csv('data/NL_BO_SENSE_2021.csv')
+df3 = pd.read_csv('data/NL_BO_SENSE_202012.csv')
