78bc8d7603d6fe65d0a5a10a163706f11315a911
jaehoon-Han <eroel1060@daum.net>
2022-05-25 15:31:46 +0900

    Update app_data.py

--- app_data.py
+++ app_data.py
@@ -2,5 +2,4 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-best_book = pd.read_csv('data/best_book_2021_12.csv')
-best_book =best_book.loc[:,['title','author','description','img_url','rank']]
+
