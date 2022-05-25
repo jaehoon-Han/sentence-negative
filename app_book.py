ae1bd42df7492774300623bbd057aa760a89f504
jaehoon-Han <eroel1060@daum.net>
2022-05-25 15:26:26 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -13,5 +13,5 @@ def run_book() :
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
-    st.dataframe('data/best_book_2021_12.csv')
+    st.table('data/best_book_2021_12.csv')
 
