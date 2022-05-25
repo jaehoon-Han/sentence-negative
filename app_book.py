ab1b13105313459b506f5013f790f9cf4b637a3c
jaehoon-Han <eroel1060@daum.net>
2022-05-25 14:44:24 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -8,6 +8,6 @@ best_book = best_book.loc[:,['title','author','description','img_url','rank']]
 
 def run_book() :
 
-    st.image('http://image.aladin.co.kr/product/5100/19/cover/8925554992_1.jpg')
+    st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
