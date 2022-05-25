b861bedc60aeba0a66293b595ef7bee1180b2f4f
jaehoon-Han <eroel1060@daum.net>
2022-05-25 16:45:25 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -7,18 +7,19 @@ best_book = pd.read_csv('data/best_book_2021_12.csv')
 best_book.dropna(axis=0, subset = ['description','img_url'],inplace = True)
 best_book = best_book.loc[:,['title','author','description','img_url','age']]
 best_book_not = best_book.loc[:,['title','author','description','age']]
+
+
+
+best_book = best_book['age'].dropna()
 df_index1 = best_book.loc[best_book['age'].str.contains('청소년')== True, ].index
 df_index2 = best_book.loc[best_book['age'].str.contains('영유아')== True, ].index
 df_index3 = best_book.loc[best_book['age'].str.contains('유아')== True, ].index
 df_index4 = best_book.loc[best_book['age'].str.contains('초등')== True, ].index
-
-best_book.drop(df_index1, inplace= True)
-best_book.drop(df_index2, inplace= True)
-best_book.drop(df_index3, inplace= True)
-best_book.drop(df_index4, inplace= True)
-best_book = best_book['age'].dropna()
-
-best_book.drop_duplicates('title')
+best_book = best_book.drop(df_index1)
+best_book = best_book.drop(df_index2)
+best_book = best_book.drop(df_index3)
+best_book = best_book.drop(df_index4)
+best_book = best_book.drop_duplicates('title')
 
 
 def run_book() :
