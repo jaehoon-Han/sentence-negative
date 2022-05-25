1db1988334b6cae817bfe035d1ba79dc37c59581
jaehoon-Han <eroel1060@daum.net>
2022-05-25 14:52:29 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -8,6 +8,10 @@ best_book = best_book.loc[:,['title','author','description','img_url','rank']]
 
 def run_book() :
 
+    
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
+    
+
+
