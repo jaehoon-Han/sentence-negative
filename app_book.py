fa0858c5f0f9f6e2c56ce96bd2401e7e7bcfe9ae
jaehoon-Han <eroel1060@daum.net>
2022-05-25 11:45:25 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -5,3 +5,4 @@ import streamlit as st
 def run_book() :
     st.dataframe(pd.read_csv('data/best_book_2021_12.csv'))
     st.image('http://image.aladin.co.kr/product/5100/19/cover/8925554992_1.jpg')
+    st.text()
