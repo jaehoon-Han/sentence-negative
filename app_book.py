794400c3d40e452ea756895e4d282a01e7455247
jaehoon-Han <eroel1060@daum.net>
2022-05-25 16:47:14 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -3,16 +3,17 @@ import streamlit as st
 import random
 
 
-best_book = pd.read_csv('data/best_book_2021_12.csv')
-best_book.dropna(axis=0, subset = ['description','img_url'],inplace = True)
-best_book = best_book.loc[:,['title','author','description','img_url','age']]
-best_book_not = best_book.loc[:,['title','author','description','age']]
+
 
 
 
 
 
 def run_book() :
+    best_book = pd.read_csv('data/best_book_2021_12.csv')
+    best_book.dropna(axis=0, subset = ['description','img_url'],inplace = True)
+    best_book = best_book.loc[:,['title','author','description','img_url','age']]
+    best_book_not = best_book.loc[:,['title','author','description','age']]
     best_book = best_book['age'].dropna()
     df_index1 = best_book.loc[best_book['age'].str.contains('청소년')== True, ].index
     df_index2 = best_book.loc[best_book['age'].str.contains('영유아')== True, ].index
