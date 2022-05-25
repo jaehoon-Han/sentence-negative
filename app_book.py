657728b6f06958294bcde63dfc4d2d16668db10d
jaehoon-Han <eroel1060@daum.net>
2022-05-25 16:46:08 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -10,19 +10,19 @@ best_book_not = best_book.loc[:,['title','author','description','age']]
 
 
 
-best_book = best_book['age'].dropna()
-df_index1 = best_book.loc[best_book['age'].str.contains('청소년')== True, ].index
-df_index2 = best_book.loc[best_book['age'].str.contains('영유아')== True, ].index
-df_index3 = best_book.loc[best_book['age'].str.contains('유아')== True, ].index
-df_index4 = best_book.loc[best_book['age'].str.contains('초등')== True, ].index
-best_book = best_book.drop(df_index1)
-best_book = best_book.drop(df_index2)
-best_book = best_book.drop(df_index3)
-best_book = best_book.drop(df_index4)
-best_book = best_book.drop_duplicates('title')
 
 
 def run_book() :
+    best_book = best_book['age'].dropna()
+    df_index1 = best_book.loc[best_book['age'].str.contains('청소년')== True, ].index
+    df_index2 = best_book.loc[best_book['age'].str.contains('영유아')== True, ].index
+    df_index3 = best_book.loc[best_book['age'].str.contains('유아')== True, ].index
+    df_index4 = best_book.loc[best_book['age'].str.contains('초등')== True, ].index
+    best_book = best_book.drop(df_index1)
+    best_book = best_book.drop(df_index2)
+    best_book = best_book.drop(df_index3)
+    best_book = best_book.drop(df_index4)
+    best_book = best_book.drop_duplicates('title')
 
     
     st.image(best_book.loc[random.randint(0,2136), 'img_url'])
