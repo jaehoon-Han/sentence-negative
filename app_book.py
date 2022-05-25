417e91c81837bf4954de96fbe958b4726baff044
jaehoon-Han <eroel1060@daum.net>
2022-05-25 14:43:45 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -11,4 +11,3 @@ def run_book() :
     st.image('http://image.aladin.co.kr/product/5100/19/cover/8925554992_1.jpg')
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
     st.image(best_book.loc[random.randint(0,97988), 'img_url'])
-    st.dataframe(best_book)
