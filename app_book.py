1e85e4b5da1ee70e9e4174ac3fa70d2e028ed0d7
jaehoon-Han <eroel1060@daum.net>
2022-05-26 10:48:35 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -14,8 +14,8 @@ def run_book() :
     best_book20 = best_book[best_book['age'].str.contains('20대')==True]
     best_book30 = best_book[best_book['age'].str.contains('30대')==True]
     best_book2030 = pd.concat([best_book20,best_book30]).reset_index()
-    st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]/3), 'img_url'])
-    st.image(best_book2030.loc[random.randint(best_book2030.shape[0]/3,(best_book2030.shape[0]/3)*2), 'img_url'])
-    st.image(best_book2030.loc[random.randint((best_book2030.shape[0]/3)*2,best_book2030.shape[0]), 'img_url'])
+    st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]), 'img_url'])
+    st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]), 'img_url'])
+    st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]), 'img_url'])
     st.dataframe(best_book2030)
 
