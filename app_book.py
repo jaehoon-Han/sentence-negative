854498906cb0d6d13c6772795874924cbf605921
jaehoon-Han <eroel1060@daum.net>
2022-05-26 10:52:40 +0900

    Update app_book.py

--- app_book.py
+++ app_book.py
@@ -7,7 +7,7 @@ def run_book() :
     best_book = pd.read_csv('data/best_book_2021_12.csv')
     best_book.dropna(axis=0, subset = ['description','img_url'],inplace = True)
     best_book = best_book.loc[:,['title','author','description','img_url','age']]
-    best_book_not = best_book.loc[:,['title','author','description','age']]
+    best_book_not = best_book2030.loc[:,['title','author','description','age']]
     best_book['age'] = best_book['age'].dropna()
 
     
@@ -18,5 +18,5 @@ def run_book() :
     st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]), 'img_url'])
     st.image(best_book2030.loc[random.randint(0,best_book2030.shape[0]), 'img_url'])
          
-    st.dataframe(best_book2030)
+    st.dataframe(best_book2030_not)
 
